ca501d8f06829e5bd30f2d2c2a8c19cb11c44f6f
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-09-10 09:56:03 -0500

    app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import './App.css';
+import Homepage from './Homepage/Homepage';
+import {Route, Link} from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  renderMainRoutes() {
+    return (
+      <Route 
+      exact
+      path='/'
+      render={() => {
+        return (<Homepage />)
+      }}
+      />
+    )
+  }
+
+  render() {
+    return (
+      <div id="App">
+        <main>
+        {this.renderMainRoutes()}
+        </main>
+      </div>
+    )
+  }
 }
+
 
 export default App;
